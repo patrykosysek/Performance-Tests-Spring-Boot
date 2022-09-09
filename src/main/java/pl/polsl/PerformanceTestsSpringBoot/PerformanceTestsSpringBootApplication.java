b04955af263a52e7a97972dfc3644305da65daf5
Patryk <patryk.osysek@itsilesia.com>
2022-09-09 12:52:44 +0200

    init

---
+++ src/main/java/pl/polsl/PerformanceTestsSpringBoot/PerformanceTestsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.PerformanceTestsSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTestsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTestsSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/polsl/PerformanceTestsSpringBoot/PerformanceTestsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.PerformanceTestsSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceTestsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
