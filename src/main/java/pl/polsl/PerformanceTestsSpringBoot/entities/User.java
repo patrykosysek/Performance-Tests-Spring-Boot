b1d0999afd3e4b7307fecc6a66d1b3b1be5d8d94
Patryk <patryk.osysek@itsilesia.com>
2022-09-09 13:35:51 +0200

    configuration

---
+++ src/main/java/pl/polsl/PerformanceTestsSpringBoot/entities/User.java
@@ -0,0 +1,24 @@
+package pl.polsl.PerformanceTestsSpringBoot.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "\"user\"")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false, nullable = false, unique = true)
+    protected UUID id;
+
+}
